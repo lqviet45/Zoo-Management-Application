6968854bb26466ebea511fb617803b545aaa1629
lqviet45 <lqviet455@gmail.com>
2023-10-05 22:23:55 +0700

    fix bugs when merge

--- Zoo-Management-Application/Program.cs
+++ Zoo-Management-Application/Program.cs
@@ -113,15 +113,6 @@ builder.Services.AddCors(options =>
 });
 
 // Add authentication to Server
-
-builder.Services.AddAuthentication(options =>
-{
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-
-	options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-});
-// Add authentication to Server
-
 builder.Services.AddAuthentication(options => 
 { 
 	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
