7182c2a0532540f01eafe4091c72317c548dafc7
lqviet45 <lqviet455@gmail.com>
2023-10-29 09:52:22 +0700

    adding custom folder when upload

--- ServiceContracts/IFirebaseStorageService.cs
+++ ServiceContracts/IFirebaseStorageService.cs
@@ -9,6 +9,21 @@ namespace ServiceContracts
 {
 	public interface IFirebaseStorageService
 	{
+		/// <summary>
+		/// Uploading the img to FireBase
+		/// </summary>
+		/// <param name="name">The img name</param>
+		/// <param name="file">The file to upload</param>
+		/// <returns>A uri of the img upload</returns>
 		Task<string> UploadFile(string name, IFormFile file);
+
+		/// <summary>
+		/// Uploading the img to FireBase this function allow you to choose where to save image to FireBase
+		/// </summary>
+		/// <param name="name">The img name</param>
+		/// <param name="file">The file to upload</param>
+		/// <param name="folderSave">The folder to save the image if the folder is not exist it will auto create</param>
+		/// <returns>A uri of the img upload</returns>
+		Task<string> UploadFile(string name, IFormFile file, string folderSave);
 	}
 }
--- Services/FirebaseStorageService.cs
+++ Services/FirebaseStorageService.cs
@@ -1,17 +1,13 @@
 ﻿using Google.Cloud.Storage.V1;
 using Microsoft.AspNetCore.Http;
 using ServiceContracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services
 {
 	public class FirebaseStorageService : IFirebaseStorageService
 	{
 		private readonly StorageClient _storageClient;
+		// the bucketName the sava on firebase
 		private const string BucketName = "zoo-management-application.appspot.com";
 
         public FirebaseStorageService(StorageClient storageClient)
@@ -21,14 +17,36 @@ namespace Services
 
         public async Task<string> UploadFile(string name, IFormFile file)
 		{
+			// Create the Guid to make the name of image unique
 			var randomGuid = Guid.NewGuid();
 
 			using var stream = new MemoryStream();
 			await file.CopyToAsync(stream);
 
+			// Uploading image to FireBase
 			var image = await _storageClient.UploadObjectAsync(BucketName,
 				$"images/ticket/{name}-{randomGuid}", file.ContentType, stream);
 
+			//Get the image URI to get on the client the img
+			var photoUri = image.MediaLink;
+
+			return photoUri;
+		}
+
+		//This is the test method please note this when you use
+		public async Task<string> UploadFile(string name, IFormFile file, string folderSave)
+		{
+			// Create the Guid to make the name of image unique
+			var randomGuid = Guid.NewGuid();
+
+			using var stream = new MemoryStream();
+			await file.CopyToAsync(stream);
+
+			// Uploading image to FireBase
+			var image = await _storageClient.UploadObjectAsync(BucketName,
+				$"{folderSave}/{name}-{randomGuid}", file.ContentType, stream);
+
+			//Get the image URI to get on the client the img
 			var photoUri = image.MediaLink;
 
 			return photoUri;
